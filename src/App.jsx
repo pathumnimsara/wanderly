5da322b7cd707987db247554e79177eb90a80d7a
PathumNimsara <pathumnimesh253@gmail.com>
2026-09-22 10:58:08 +0530

    feat: add about section and premium polish

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 
+import About from "./components/About";
 import Destinations from "./components/Destinations";
 
 
@@ -9,14 +10,14 @@ function App() {
       <nav>
         <div className="logo">WANDERLY</div>
 
-        <div className="links">
-          <a href="#">Discover</a>
-          <a href="#">Destinations</a>
-          <a href="#">About</a>
-        </div>
+       <div className="links">
+  <a href="#discover">Discover</a>
+  <a href="#destinations">Destinations</a>
+  <a href="#about">About</a>
+</div>
       </nav>
 
-      <section className="hero">
+      <section className="hero" id="discover">
         <div className="hero-text">
           <p className="location">📍 Dolomites, Italy</p>
 
@@ -36,6 +37,15 @@ function App() {
       </section>
 
      <Destinations /> 
+     <About />
+    <footer>
+  <div className="footer-logo">WANDERLY</div>
+
+  <div className="footer-text">
+    © 2026 Wanderly. Explore more.
+  </div>
+</footer>
+
     </div>
   );
 }
